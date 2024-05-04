abda9615ec51927267f123cc3d7f5b7796c26c87
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2024-05-04 18:24:48 +0800

    updated code

--- js/scripts.js
+++ js/scripts.js
@@ -20,14 +20,14 @@ var typed = new Typed(typingElement, {
     loop: true
 });
 
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('mainNav');
-    var header = document.querySelector('header');
-    var headerHeight = header.offsetHeight;
+// window.addEventListener('scroll', function() {
+//     var navbar = document.getElementById('mainNav');
+//     var header = document.querySelector('header');
+//     var headerHeight = header.offsetHeight;
 
-    if (window.pageYOffset > headerHeight) {
-        navbar.classList.add('sticky-top');
-    } else {
-        navbar.classList.remove('sticky-top');
-    }
-});
+//     if (window.pageYOffset > headerHeight) {
+//         navbar.classList.add('sticky-top');
+//     } else {
+//         navbar.classList.remove('sticky-top');
+//     }
+// });
