10faa61b9878d923a67167838ef520c8bdaaca72
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2024-06-02 14:51:27 +0800

    edited footer

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,17 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
 */
 // Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
+// window.addEventListener('DOMContentLoaded', event => {
+//     const listHoursArray = document.body.querySelectorAll('.list-hours li');
+//     listHoursArray[new Date().getDay()].classList.add(('today'));
 
-});
+// });
 
 var typingElement = document.querySelector('.typing');
 
 var typed = new Typed(typingElement, {
-    //strings: ["Student", "Web Developer", "UI/UX Designer", "Cybersecurity Enthusiast"],
-    strings: ["Student", "Web Developer", "UI/UX Designer"],
+    strings: ["Student", "Web Developer", "UI/UX Designer", "Cybersecurity Enthusiast"],
+    //strings: ["Student", "Web Developer", "UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
@@ -31,26 +31,3 @@ var typed = new Typed(typingElement, {
 //         navbar.classList.remove('sticky-top');
 //     }
 // });
-
-
-// function openModal(image) {
-//     // Create a modal element
-//     var modal = document.createElement('div');
-//     modal.className = 'modal';
-    
-//     // Create an image element to display the larger image
-//     var modalImg = document.createElement('img');
-//     modalImg.src = image.src;
-//     modalImg.alt = image.alt;
-    
-//     // Append the image to the modal
-//     modal.appendChild(modalImg);
-    
-//     // Append the modal to the body
-//     document.body.appendChild(modal);
-    
-//     // Close the modal when clicked outside of the image
-//     modal.onclick = function() {
-//       modal.remove();
-//     };
-//   }
