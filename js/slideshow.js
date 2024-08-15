661923e7785f8f6aebd50c084778ec88be6008d5
joyyyyyyyyyyyyyyyyyy <22022096@myrp.edu.sg>
2024-08-15 13:37:26 +0800

    -

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,14 +1,23 @@
+// initialize slideIndex to 1, call showslide func w slideindex argument
 let slideIndex = 1;
 showSlides(slideIndex);
 
+//used to move to next or previous slide, by adding or subtracting value of slideindex
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+//used to display img corresponding to certain slide index
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+//used to display img corresponding to current slideindex
+//slides n dots variable are set to the html elements with the corresponding class name
+//1st if statement checks if n is greater than the length of slides, if yes, resets the value of slideindex to 1
+//2nd if statement checks if the value of n is less than 1, if yes, sets the slideindex to length of slide, eg 1
+//1st for loop is used to hide all the slides by setting their display CSS property to none
+//2nd for loop is used to remove the active class from all the dots.
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
